cba2ad57991e14d9035b66f1751a830157538671
Yeon Woong Lim <woong232@gmail.com>
2020-09-05 13:42:06 +0900

    origin

---
+++ server.ino
@@ -0,0 +1,147 @@
+/*
+ * ESP8266 SPIFFS HTML Web Page with JPEG, PNG Image 
+ *
+ */
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>   //Include File System Headers
+
+const char* imagefile = "/image.png";
+const char* htmlfile = "/index.html";
+
+//ESP AP Mode configuration
+const char *ssid = "dropGT";
+const char *password = "98983084";
+
+
+ESP8266WebServer server(80);
+
+void handleRoot(){
+  server.sendHeader("Location", "/index.html",true);   //Redirect to our html web page
+  server.send(302, "text/plane","");
+}
+
+void handleWebRequests(){
+  if(loadFromSpiffs(server.uri())) return;
+  String message = "File Not Detected\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " NAME:"+server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  Serial.println(message);
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+
+  //Initialize File System
+  SPIFFS.begin();
+  Serial.println("File System Initialized");
+
+  //Initialize AP Mode
+  WiFi.softAP(ssid, password);  //Password not used
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("Web Server IP:");
+  Serial.println(myIP);
+
+  //Initialize Webserver
+  server.on("/",handleRoot);
+  server.on("/ledRED",ledRED); //Set setver all paths are not found so we can handle as per URI
+  server.on("/ledGREEN",ledGREEN);
+  server.on("/ledBLUE",ledBLUE);
+  server.on("/ledCYAN",ledCYAN);
+  server.on("/ledYELLOW",ledYELLOW);
+  server.on("/ledMAGENTA",ledMAGENTA);
+  server.on("/ledWHITE",ledWHITE);
+  server.on("/ledOFF",ledOFF);
+  server.onNotFound(handleWebRequests);
+  server.begin();  
+}
+
+void ledGREEN(){ //초록
+  analogWrite(D4,255); //초록
+  analogWrite(D3,0); //레드
+  analogWrite(D2,0); // 블루
+  server.send(204,"");
+}
+void ledBLUE(){ // 블루
+  analogWrite(D4,0);
+  analogWrite(D3,0);
+  analogWrite(D2,255);
+  server.send(204,"");  
+}
+void ledRED(){ // 레드 
+  analogWrite(D4,0);
+  analogWrite(D3,255);
+  analogWrite(D2,0);
+  server.send(204,"");  
+}
+void ledCYAN(){
+  analogWrite(D4,255);
+  analogWrite(D3,0);
+  analogWrite(D2,255);
+  server.send(204,"");  
+}
+void ledYELLOW(){
+  analogWrite(D4,255);
+  analogWrite(D3,255);
+  analogWrite(D2,0);
+  server.send(204,"");
+}
+void ledMAGENTA(){
+  analogWrite(D4,0);
+  analogWrite(D3,255);
+  analogWrite(D2,255);
+  server.send(204,"");
+}
+void ledWHITE(){
+  analogWrite(D4,255);
+  analogWrite(D3,255);
+  analogWrite(D2,255);
+  server.send(204,"");
+}
+void ledOFF(){
+  analogWrite(D4,0);
+  analogWrite(D3,0);
+  analogWrite(D2,0);
+  server.send(204,"");
+}
+
+void loop() {
+ server.handleClient();
+}
+
+bool loadFromSpiffs(String path){
+  String dataType = "text/plain";
+  if(path.endsWith("/")) path += "index.htm";
+
+  if(path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
+  else if(path.endsWith(".html")) dataType = "text/html";
+  else if(path.endsWith(".htm")) dataType = "text/html";
+  else if(path.endsWith(".css")) dataType = "text/css";
+  else if(path.endsWith(".js")) dataType = "application/javascript";
+  else if(path.endsWith(".png")) dataType = "image/png";
+  else if(path.endsWith(".gif")) dataType = "image/gif";
+  else if(path.endsWith(".jpg")) dataType = "image/jpeg";
+  else if(path.endsWith(".ico")) dataType = "image/x-icon";
+  else if(path.endsWith(".xml")) dataType = "text/xml";
+  else if(path.endsWith(".pdf")) dataType = "application/pdf";
+  else if(path.endsWith(".zip")) dataType = "application/zip";
+  File dataFile = SPIFFS.open(path.c_str(), "r");
+  if (server.hasArg("download")) dataType = "application/octet-stream";
+  if (server.streamFile(dataFile, dataType) != dataFile.size()) {
+  }
+
+  dataFile.close();
+  return true;
+}
